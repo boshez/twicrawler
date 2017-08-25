8967f6826bf9a1abb1448763522e7a6aacc8842c
Yuya Takashina <takashina2051@gmail.com>
2017-08-25 21:31:34 +0900

    整理

--- twicrawler.py
+++ twicrawler.py
@@ -4,23 +4,16 @@ import pqdict
 
 
 class Crawler(object):
-    def __init__(self, api, seed):
-        self.api = api
-        self.api.wait_on_rate_limit = True
+    def __init__(self, seed):
         # the larger the more important (reverse=True)
         self.crawl_frontier = pqdict.pqdict({user_id: 0 for user_id in seed}, reverse=True)
         self.visited = []
         self.edges = []
         self.graph = nx.DiGraph()
 
-    def _fetch_user(self, user_id):
-        return self.api.get_user(user_id)
-
-    def _fetch_friends_ids(self, user_id):
-        return self.api.friends_ids(user_id)
-
-    def crawl(self, update_interval=5, max_itr=15):
+    def crawl(self, api, update_interval=5, max_itr=15):
         cnt = 0
+        api.wait_on_rate_limit = True
         while cnt < max_itr:
             if not self.crawl_frontier:
                 break
@@ -30,12 +23,12 @@ class Crawler(object):
                 continue
             self.visited.append(user_id)
 
-            status = self._fetch_user(user_id)
+            status = api.get_user(user_id)
             if status.protected:
                 continue
             print('user_id: %d, screen_name: %s' % (user_id, status.screen_name))
 
-            friends = self._fetch_friends_ids(user_id)
+            friends = api.friends_ids(user_id)
             self.edges.extend([(user_id, friend) for friend in friends])
             self.crawl_frontier.update({friend: -1 for friend in friends if friend not in self.crawl_frontier})
 

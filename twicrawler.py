0b884cc2314a1a8a6e5599ef52a35f9d0ecb8f70
Yuya Takashina <takashina2051@gmail.com>
2017-08-25 19:05:52 +0900

    modified:   twicrawler.py

--- twicrawler.py
+++ twicrawler.py
@@ -4,21 +4,14 @@ import pqdict
 
 
 class Crawler(object):
-    def __init__(self, config, seed):
-        seed_dict = {user_id: 0 for user_id in seed}
+    def __init__(self, api, seed):
+        self.api = api
+        self.api.wait_on_rate_limit = True
         # the larger the more important (reverse=True)
-        self.crawl_frontier = pqdict.pqdict(seed_dict, reverse=True)
+        self.crawl_frontier = pqdict.pqdict({user_id: 0 for user_id in seed}, reverse=True)
         self.visited = []
         self.edges = []
-        self.graph = None
-        self.statuses = []
-        self._auth_api(config)
-
-    def _auth_api(self, config):
-        auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-        auth.set_access_token(config.access_token_key, config.access_token_secret)
-        self.api = tweepy.API(auth)
-        self.api.wait_on_rate_limit = True
+        self.graph = nx.DiGraph()
 
     def _fetch_user(self, user_id):
         return self.api.get_user(user_id)
@@ -40,7 +33,6 @@ class Crawler(object):
             status = self._fetch_user(user_id)
             if status.protected:
                 continue
-            self.statuses.append(status)
             print('user_id: %d, screen_name: %s' % (user_id, status.screen_name))
 
             friends = self._fetch_friends_ids(user_id)
@@ -50,7 +42,8 @@ class Crawler(object):
             cnt += 1
 
             if cnt % update_interval == 0:
-                self.graph = nx.from_edgelist(self.edges)
+                self.graph.add_edges_from(self.edges)
+                self.edges[:] = []
                 ranks = nx.pagerank(self.graph)
                 for key in self.crawl_frontier.keys():
                     if key in ranks:
